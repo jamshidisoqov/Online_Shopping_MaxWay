67904f570804c2a3ad7ee5a86dca5f4868742d3b
jamshidisoqov <isoqovjamshid01@gmail.com>
2022-10-21 10:51:02 +0500

    settings service screen created

---
+++ app/src/main/java/uz/gita/online_shopping/presentation/screens/settings/SettingsScreen.kt
@@ -0,0 +1,8 @@
+package uz.gita.online_shopping.presentation.screens.settings
+
+import androidx.fragment.app.Fragment
+import uz.gita.online_shopping.R
+
+// Created by Jamshid Isoqov an 10/21/2022
+class SettingsScreen:Fragment(R.layout.screen_settings) {
+}
